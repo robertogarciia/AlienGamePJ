e5af520860dfa913f62f26a929d1de540007dba9
Nico Reyes <nicolau.reyes@mataro.epiaedu.cat>
2023-09-28 20:36:47 +0200

    Noves clases

---
+++ ProjecteAlien/ProjecteAlien/src/Alhilal.java
@@ -0,0 +1,19 @@
+public class Alhilal {
+
+    private int tellthetruth;
+
+    public Alhilal(int tellthetruth) {
+        this.tellthetruth = tellthetruth;
+    }
+
+    public int getTellthetruth() {
+        return tellthetruth;
+    }
+
+    public void setTellthetruth(int tellthetruth) {
+        this.tellthetruth = tellthetruth;
+    } 
+
+    
+    
+}
---
+++ ProjecteAlien/ProjecteAlien/src/Alien.java
@@ -0,0 +1,18 @@
+public class Alien {
+
+    private Boolean alive;
+
+    public Alien(Boolean alive) {
+        this.alive = alive;
+    }
+
+    public Boolean getAlive() {
+        return alive;
+    }
+
+    public void setAlive(Boolean alive) {
+        this.alive = alive;
+    }
+    
+    
+}
